4fc3251dcd2551092036fc6680b5995860b459f7
dkout <DimitryKoutchine@cmail.carleton.ca>
2022-03-04 16:32:23 -0500

    Project

--- src/main/java/main/ServingWebContentApplication.java
+++ src/main/java/main/ServingWebContentApplication.java
@@ -2,9 +2,10 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServingWebContentApplication {
+public class ServingWebContentApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
