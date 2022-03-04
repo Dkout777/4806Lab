86fbe75e1a84cc2256ab90447c0971f6405299db
dkout <DimitryKoutchine@cmail.carleton.ca>
2022-03-04 17:35:49 -0500

    Project

---
+++ src/test/java/main/TestingWebApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplication{
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
